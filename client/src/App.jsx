4a44c83ab3132a5fe1667207a71913cfa7366ad2
ritesh <riteshchaudhary375@gmail.com>
2024-06-22 17:39:11 +0545

    update route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
-import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/home" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
