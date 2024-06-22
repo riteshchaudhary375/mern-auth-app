7ee70bde52037b354691655fc67c0f0a4d3be27a
ritesh <riteshchaudhary375@gmail.com>
2024-06-22 17:39:38 +0545

    complete header component ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-sky-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold">MERN Auth</h1>
+        </Link>
+        <ul className="flex gap-5">
+          <Link to="/sign-in" className="hover:underline">
+            <li>Sign in</li>
+          </Link>
+          <Link to="/sign-up" className="hover:underline">
+            <li>Sign up</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
