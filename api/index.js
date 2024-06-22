59291d5666aa7d7d28ee16ffca2917917288180c
ritesh <riteshchaudhary375@gmail.com>
2024-06-22 18:44:33 +0545

    create express app and connect to mongodb database

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
