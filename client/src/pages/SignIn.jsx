c8647b555825ae6c331cfdcfc25f71774894895f
ritesh <riteshchaudhary375@gmail.com>
2024-06-23 22:10:31 +0545

    complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,26 +1,75 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (
+      !formData.email ||
+      !formData.password ||
+      formData.email === "" ||
+      formData.password === ""
+    ) {
+      return setErrorMessage("All fields are required");
+    }
+
+    try {
+      setErrorMessage(false);
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <div className="border rounded-2xl shadow p-6 mt-7">
         <h1 className="text-3xl text-center font-semibold mb-7">Sign In</h1>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <input
             type="email"
             placeholder="Email"
             id="email"
             className="border-2 p-3 rounded-lg outline-sky-500"
+            onChange={handleChange}
           />
           <input
             type="password"
             placeholder="Password"
             id="password"
             className="border-2 p-3 rounded-lg outline-sky-500"
+            onChange={handleChange}
           />
-          <button className="bg-sky-600 text-white p-3 rounded-lg hover:opacity-90  disabled:opacity-80">
-            Sign In
+          <button
+            disabled={loading}
+            className="bg-sky-600 text-white p-3 rounded-lg hover:opacity-90  disabled:opacity-80"
+          >
+            {loading ? "Loading..." : "Sign In"}
           </button>
         </form>
         <div className="flex gap-2 mt-5">
@@ -29,6 +78,7 @@ const SignIn = () => {
             <span className="text-blue-500 hover:underline">Sign up</span>
           </Link>
         </div>
+        <p className="text-red-500 mt-5">{errorMessage && errorMessage}</p>
       </div>
     </div>
   );
