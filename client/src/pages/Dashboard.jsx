c83a76877dd95a6743aa9ec917e0c5bdd582016b
ritesh <riteshchaudhary375@gmail.com>
2024-06-26 12:13:10 +0545

    update user dashboard ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   const { currentUser } = useSelector((state) => state.user);
@@ -10,29 +11,68 @@ const Dashboard = () => {
         Welcome back,
       </span>
       <div className="mt-10 max-w-sm mx-auto border border-gray-200 rounded-lg shadow-md flex flex-col items-center p-5">
-        <div className="relative h-32 w-32 self-center cursor-pointer shadow-lg overflow-hidden rounded-full">
+        <div className="relative h-32 w-32 my-3 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
           <img
             src={currentUser.profilePicture}
             alt="user-profile"
             className="rounded-full w-full h-full object-cover border-4 border-[lightgray]"
           />
         </div>
-        <h1 className="font-semibold text-3xl text-slate-700 underline mt-6 mb-4">
+        <h1 className="font-semibold text-3xl text-slate-700 underline mb-4">
           Profile
         </h1>
-        <div className="font-semibold text-lg">
-          <p>
-            <span className="text-slate-600">Username: </span>
-            {currentUser.username}
-          </p>
-          <p>
-            <span className="text-slate-600">Email: </span>
-            {currentUser.email}
-          </p>
+        <form className="w-full">
+          <div className="flex items-center justify-between mb-2">
+            <label htmlFor="username" className="text-slate-500 w-1/4">
+              Username:{" "}
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="text-slate-700 border border-slate-300 outline-slate-300 p-1 rounded-md  w-3/4"
+              value={currentUser.username}
+            />
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <label htmlFor="email" className="text-slate-500 w-1/4">
+              Email:{" "}
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="text-slate-700 border border-slate-300 outline-slate-300 p-1 rounded-md  w-3/4"
+              value={currentUser.email}
+            />
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <label htmlFor="password" className="text-slate-500 w-1/4">
+              Password:{" "}
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="text-slate-700 border border-slate-300 outline-slate-300 p-1 rounded-md  w-3/4"
+              placeholder="********"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <button className="font-semibold text-xl bg-sky-700 text-white px-4 py-1 rounded-lg mt-3 hover:opacity-85">
+              Update Profile
+            </button>
+          </div>
+        </form>
+        <hr className="w-full my-4" />
+        <div className="w-full flex flex-row items-center justify-between">
+          <Link to="#" className="cursor-pointer hover:underline">
+            <p>Delete Account?</p>
+          </Link>
+          <Link to="#" className="cursor-pointer hover:underline">
+            <p>Sign out</p>
+          </Link>
         </div>
-        <button className="font-semibold text-xl bg-red-700 text-white px-4 py-1 rounded-lg mt-5 hover:opacity-85">
-          Log out
-        </button>
       </div>
     </div>
   );
