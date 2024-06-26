b76a7aa26ddb14baf9f9ce3ddc991c97ab911bd6
ritesh <riteshchaudhary375@gmail.com>
2024-06-26 17:30:53 +0545

    update header ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,9 +12,37 @@ const Header = () => {
           <h1 className="font-bold">MERN Auth</h1>
         </Link>
         <ul className="flex gap-5">
-          {currentUser ? (
-            <Link to="/dashboard" className="hover:underline">
-              <li>Profile</li>
+          <Link to="/" className="hover:underline">
+            <li>Home</li>
+          </Link>
+          <Link to="/dashboard">
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                alt="profile"
+                className="h-7 w-7 rounded-full object-cover"
+                title="Profile"
+              />
+            ) : (
+              <div className="flex gap-4">
+                <Link to="/sign-in" className="hover:underline">
+                  <li>Sign in</li>
+                </Link>
+                <Link to="/sign-up" className="hover:underline">
+                  <li>Sign up</li>
+                </Link>
+              </div>
+            )}
+          </Link>
+
+          {/* {currentUser ? (
+            <Link to="/dashboard">
+              <img
+                src={currentUser.profilePicture}
+                alt="profile"
+                className="h-7 w-7 rounded-full object-cover"
+                title="Profile"
+              />
             </Link>
           ) : (
             <>
@@ -25,7 +53,7 @@ const Header = () => {
                 <li>Sign up</li>
               </Link>
             </>
-          )}
+          )} */}
         </ul>
       </div>
     </div>
